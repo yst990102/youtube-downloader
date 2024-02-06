b6176768960d8e071600e50dd3b8ec3ea9a72a18
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2024-02-07 04:30:39 +1100

    开启CORS跨域名

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, redirect, render_template, jsonify
+from flask_cors import CORS
 from pytube import YouTube
 from pathlib import Path
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
